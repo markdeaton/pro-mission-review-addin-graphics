41da9083807a1b8a38864dc24304a948e80b7e35
markdeaton <medeaton@hotmail.com>
2020-08-11 16:54:23 -0700

    * Added combo * Added logic to populate combo on change to selected TOC feature layer

---
+++ MissionAgentReview/AgentsCombo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace MissionAgentReview {
+    /// <summary>
+    /// Represents the ComboBox
+    /// </summary>
+    internal class AgentsCombo : ComboBox {
+
+        private bool _isInitialized;
+
+        /// <summary>
+        /// Combo Box constructor
+        /// </summary>
+        public AgentsCombo() {
+            Module1.Current.PropertyChanged += Module1_PropertyChanged;
+            UpdateCombo();
+        }
+
+        /// <summary>
+        /// Updates the combo box with all the items.
+        /// </summary>
+
+        private void UpdateCombo() {
+            Clear();
+            foreach (string agentName in Module1.AgentList) {
+                Add(new ComboBoxItem(agentName));
+            }
+            _isInitialized = true;
+
+
+            //Enabled = true; //enables the ComboBox
+            //SelectedItem = ItemCollection.FirstOrDefault(); //set the default item in the comboBox
+
+        }
+
+        private void Module1_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+            System.Diagnostics.Debug.WriteLine("Property changed");
+            if (e.PropertyName == Module1.PROP_AGENTLIST) UpdateCombo();
+        }
+
+        #region Overrides
+        /// <summary>
+        /// The on comboBox selection change event. 
+        /// </summary>
+        /// <param name="item">The newly selected combo box item</param>
+        protected override void OnSelectionChange(ComboBoxItem item) {
+
+            if (item == null)
+                return;
+
+            if (string.IsNullOrEmpty(item.Text))
+                return;
+
+            // TODO  Code behavior when selection changes.
+
+        }
+        #endregion
+
+    }
+}
--- MissionAgentReview/Module1.cs
+++ MissionAgentReview/Module1.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Input;
-using System.Threading.Tasks;
-using ArcGIS.Core.CIM;
+﻿using ArcGIS.Core.CIM;
 using ArcGIS.Core.Data;
-using ArcGIS.Core.Geometry;
-using ArcGIS.Desktop.Catalog;
-using ArcGIS.Desktop.Core;
+using ArcGIS.Core.Events;
 using ArcGIS.Desktop.Editing;
-using ArcGIS.Desktop.Extensions;
 using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
-using ArcGIS.Desktop.Framework.Dialogs;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Desktop.Mapping;
 using ArcGIS.Desktop.Mapping.Events;
-using ArcGIS.Core.Events;
-using ArcGIS.Desktop.Internal.Mapping.Locate;
-using System.Windows;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MissionAgentReview {
     internal class Module1 : Module {
         private static Module1 _this = null;
         private SubscriptionToken _tocToken;
-
+        private QueryFilter _agentListQueryFilter;
+        private const string FIELD_AGENTNAME = "created_user";
+        private FeatureLayer _lyrAgentTracks;
 
         /// <summary>
         /// Retrieve the singleton instance to this module here
@@ -49,7 +45,11 @@ namespace MissionAgentReview {
 
         protected override bool Initialize() {
             _tocToken = TOCSelectionChangedEvent.Subscribe(OnTOCSelectionChanged);
-
+           
+            _agentListQueryFilter = new QueryFilter();
+            _agentListQueryFilter.PrefixClause = "DISTINCT";
+            _agentListQueryFilter.SubFields = FIELD_AGENTNAME;
+            
             return base.Initialize();
         }
         protected override void Uninitialize() {
@@ -57,56 +57,113 @@ namespace MissionAgentReview {
         }
         #endregion Overrides
 
-        private Layer _lyrAgentTracks;
+        /// <summary>
+        /// When the user selects a new TOC item, determine whether it's usable as a source for agent track analysis.
+        /// </summary>
+        /// <param name="obj">ArcGIS Pro map view reference</param>
         private void OnTOCSelectionChanged(MapViewEventArgs obj) {
             MapView mv = obj.MapView;
+            bool isButtonEnabled = false;
+            _lyrAgentTracks = null;
 
             QueuedTask.Run(() => {
                 foreach (Layer lyr in mv.GetSelectedLayers()) {
                     // look for one that has all the characteristics of a Mission agent tracks layer
-                    Task<Layer> lyrFound = isAgentTracksLayer(lyr);
+                    Task<FeatureLayer> lyrFound = isAgentTracksLayer(lyr);
                     if (lyrFound.Result != null) {
                         _lyrAgentTracks = lyrFound.Result;
+                        isButtonEnabled = true;
                         break;
                     }
                 }
-
+                if (isButtonEnabled) {
+                    FrameworkApplication.State.Activate("trackFeatureLayerSelected_state");
+                    populateAgentList();
+                } else {
+                    FrameworkApplication.State.Deactivate("trackFeatureLayerSelected_state");
+                    _agentList.Clear();
+                }
             });
 
             //throw new NotImplementedException();
             // Find and return a layer matching specs for Agent Tracks layer. Return null if not found.
-            async Task<Layer> isAgentTracksLayer(Layer lyr) {
-                Layer lyrFound = null;
+            async Task<FeatureLayer> isAgentTracksLayer(Layer lyr) {
+                FeatureLayer lyrFound = null;
                 if (lyr is FeatureLayer) {
                     bool isPointGeom, isJoinedTable, hasDataRows, hasUserField, hasCreateDateField;
 
                     FeatureLayer featLyr = (FeatureLayer)lyr;
                     isPointGeom = featLyr.ShapeType == esriGeometryType.esriGeometryPoint;
                     if (isPointGeom) await QueuedTask.Run(() => {
-                        FeatureClass fc = featLyr.GetFeatureClass();
-                        isJoinedTable = fc.IsJoinedTable();
-                        hasDataRows = fc.GetCount() > 0;
-                        if (!isJoinedTable && hasDataRows) {
-                            TableDefinition tblDef = fc.GetDefinition();
-                            IReadOnlyList<Field> fields = tblDef.GetFields();
-                            hasUserField = fields.Any((fld => fld.Name == "created_user" && fld.FieldType == FieldType.String));
-                            hasCreateDateField = fields.Any((fld => fld.Name == "created_date" && fld.FieldType == FieldType.Date));
-                            if (hasUserField && hasCreateDateField) lyrFound = lyr;
+                        try {
+                            FeatureClass fc = featLyr.GetFeatureClass();
+                            isJoinedTable = fc.IsJoinedTable();
+                            hasDataRows = fc.GetCount() > 0;
+                            if (!isJoinedTable && hasDataRows) {
+                                TableDefinition tblDef = fc.GetDefinition();
+                                IReadOnlyList<Field> fields = tblDef.GetFields();
+                                hasUserField = fields.Any((fld => fld.Name == FIELD_AGENTNAME && fld.FieldType == FieldType.String));
+                                hasCreateDateField = fields.Any((fld => fld.Name == "created_date" && fld.FieldType == FieldType.Date));
+                                if (hasUserField && hasCreateDateField) lyrFound = featLyr;
+                            }
+                        } catch (Exception e) {
+                            System.Diagnostics.Debug.Write($"Error while examining feature layer '{featLyr.Name}': {e.Message}");
                         }
                     });
                 } else if (lyr is GroupLayer) { // recursion for grouped layers
                     foreach (Layer groupedLyr in ((GroupLayer)lyr).Layers) {
-                        Task<Layer> groupedLyrResult = isAgentTracksLayer(groupedLyr);
-                        await groupedLyrResult;
-                        if (groupedLyrResult.Result != null) {
-                            lyrFound = groupedLyrResult.Result;
-                            break;
+                        try {
+                            Task<FeatureLayer> groupedLyrResult = isAgentTracksLayer(groupedLyr);
+                            await groupedLyrResult;
+                            if (groupedLyrResult.Result != null) {
+                                lyrFound = groupedLyrResult.Result;
+                                break;
+                            }
+                        } catch (Exception e) {
+                            System.Diagnostics.Debug.Write($"Error while examining group layer '{lyr.Name}': {e.Message}");
                         }
                     } // else return null because nothing was found
 
                 }
                 return lyrFound;
             }
+            void populateAgentList() {
+                if (_lyrAgentTracks == null) return;
+                _agentList.Clear();
+                QueuedTask.Run(() => {
+                    FeatureClass fc = _lyrAgentTracks.GetFeatureClass();
+                    using (RowCursor rowCur = fc.Search(_agentListQueryFilter)) {
+                        while (rowCur.MoveNext()) {
+                            using (Row row = rowCur.Current) {
+                                _agentList.Add(row[row.FindField(FIELD_AGENTNAME)].ToString()); // Assumes only one field in the query filter
+                            }
+                        }
+                    }
+                    // Raise event to notify children or clients that the list has changed
+                    NotifyPropertyChanged(PROP_AGENTLIST);
+                });
+            }
         }
+
+        #region Properties
+        public static string PROP_AGENTLIST = "Agent List";
+        private static StringCollection _agentList = new StringCollection();
+        public static StringCollection AgentList {
+            get {
+                return _agentList;
+            }
+        }
+        
+        private static string _selectedAgentName;
+        public static string SelectedAgentName {
+            get {
+                return _selectedAgentName;
+            }
+            set {
+                _selectedAgentName = value;
+            }
+        }
+
+        #endregion
     }
 }
