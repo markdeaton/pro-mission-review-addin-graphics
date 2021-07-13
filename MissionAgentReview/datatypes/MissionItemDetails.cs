85682a4ac11e28aa1aeed8a8a262f9c7b60650fd
markdeaton <medeaton@hotmail.com>
2021-07-13 13:55:38 -0700

    Support fix to Mission details retrieval logic

---
+++ MissionAgentReview/datatypes/MissionItemDetails.cs
@@ -0,0 +1,11 @@
+﻿using ArcGIS.Desktop.Core.Portal;
+
+namespace MissionAgentReview.datatypes {
+    public class MissionItemDetails {
+        public string MissionName { get; set; }
+        public PortalGroup Group { get; set; }
+        public PortalItem MissionItem { get; set; }
+        //public string FolderId { get; set; }
+        public PortalItem TracksItem { get; set; }
+    }
+}
